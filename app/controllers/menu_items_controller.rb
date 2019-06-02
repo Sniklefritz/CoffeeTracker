c25a55a9eb15919a80b0668c76a188a168cf7418
Austin <aucraig88@gmail.com>
2019-06-02 14:38:18 -0700

    Began building menu item controller. Began new, create, index, show, and private params

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,5 +1,27 @@
 class MenuItemsController < ApplicationController
+  def index
+    @menu_items = MenuItem.all
+  end
+
   def new
     @menu_item = MenuItem.new
   end
+
+  def create
+    @menu_item = MenuItem.new(menu_item_params)
+
+    if @menu_item.save
+      redirect_to @menu_item
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @menu_item = MenuItem.find(params[:id])
+  end
+
+  private
+  def menu_item_params
+    params.require(:menu_item).permit(:product_name, :required_recipe_items, :recipe_item_amount)
 end
